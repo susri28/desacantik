98ab8d08ec4dc3402b29592ff653c4f119c17546
susri28 <108443640+susri28@users.noreply.github.com>
2022-08-31 14:07:45 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("","root","","desacantik");
+$conn = mysqli_connect("localhost","root","","desacantik");
 
 ?>

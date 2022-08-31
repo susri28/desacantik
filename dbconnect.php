e19457966d7f451cd304fea35299a8f848116602
susri28 <108443640+susri28@users.noreply.github.com>
2022-08-31 14:06:51 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("https://github.com/susri28/desacantik","root","","desacantik");
+$conn = mysqli_connect("","root","","desacantik");
 
 ?>
